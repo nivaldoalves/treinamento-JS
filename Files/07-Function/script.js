e64bbfb0776f71ab8ce4ebac443fc787733dfac6
Nivaldo Alves <nivaldo.toca@gmail.com>
2023-03-04 15:49:38 -0300

    Alt file Function

--- Files/07-Function/script.js
+++ Files/07-Function/script.js
@@ -1,54 +1,63 @@
 
-function somar() {
+function f1() {
     var x = 2
     var y = 3
     var res = x + y
-    document.getElementById('res1').innerHTML = `O resultado da function somar é: ${res}`
+    document.getElementById('res1').innerHTML = `O resultado da function f1 é: ${res}`
 }
-somar()
+f1()
 
 //ou
 
-var soma = function () {
+var f2 = function () {
     var x = 2
     var y = 3
     var res = x * y
-    document.getElementById('res2').innerHTML = `O resultado da function soma é: ${res}`
+    document.getElementById('res2').innerHTML = `O resultado da function f2 é: ${res}`
 }
-soma()
+f2()
 
 //ou
 
-var som = function (x, y) {
+var f3 = function (x, y) {
     var res = x - y
-    document.getElementById('res3').innerHTML = `O resultado da function som é: ${res}`
+    document.getElementById('res3').innerHTML = `O resultado da function f3 é: ${res}`
 }
-som(20, 3)
+f3(20, 3)
 
 //ou
 
-var calc = function (x, y) {
+var f4 = function (x, y) {
     var res = x + y
     return res
 }
-var num = calc(20, 3)
-document.getElementById('res4').innerHTML = `O resultado da function calc é: ${num}`
+var num = f4(20, 3)
+document.getElementById('res4').innerHTML = `O resultado da function f4 é: ${num}`
 
 //ou
 
-function x() {
+function f5() {
     return 5 + 5
 }
-document.getElementById('res5').innerHTML = `O resultado da function x é: ${x()}`
+document.getElementById('res5').innerHTML = `O resultado da function f5 é: ${f5()}`
 
 //ou
 
-function y(a, b) {
+function f6(a, b) {
     return a + b
 }
-document.getElementById('res6').innerHTML = `O resultado da function y é: ${y(4, 4)}`
+document.getElementById('res6').innerHTML = `O resultado da function f6 é: ${f6(4, 4)}`
 
-function alerta() {
+//ou
+
+let f7 = (a, b) => {
+    return a + b
+}
+document.querySelector('#res7').innerHTML = `O resultado da function f7 é: ${f7(1, 1)}`
+
+//e
+
+const alerta = () => {
     alert('Resultado das functions')
 }
 
